fbfc9874210e54f4f2a8a84db350317ade7df324
xouwm <80478016+xouwm@users.noreply.github.com>
2021-10-18 22:12:55 +0300

    Лабораторная работа 3

--- Date.cpp
+++ Date.cpp
@@ -1,49 +1,57 @@
 #include "Date.h"
-#include<iostream>
+#include <iostream>
+
+enum Valid
+{
+    MIN_DAY = 1,
+    MIN_MONTH = 1,
+    MAX_MONTH = 12,
+    MIN_YEAR = 0,
+};
 
 void Date::set_day(const int day)
 {
-    if (day >= 1 && year <= 31)
+    if (day >= Valid::MIN_DAY && day <= days_in_month[month])
     {
         this->day = day;
         Triad::c = day;
     }
     else
     {
-        throw std::invalid_argument{ "ƒень меньше 1 или больше 31" };
+        throw std::invalid_argument{ "День меньше 1 или больше максимально возможного значения" };
     }
 }
 
 void Date::set_month(const int month)
 {
-    if (month >= 1 && year <= 12)
+    if (month >= Valid::MIN_MONTH && month <= Valid::MAX_MONTH)
     {
         this->month = month;
         Triad::b = month;
     }
     else
     {
-        throw std::invalid_argument{ "ћес¤ц меньше 1 или больше 12" };
+        throw std::invalid_argument{ "Месяц меньше 1 или больше 12" };
     }
 }
 
 void Date::set_year(const int year)
 {
-    if (year > 0)
+    if (year > Valid::MIN_YEAR)
     {
         this->year = year;
         Triad::c = year;
     }
     else
     {
-        throw std::invalid_argument{ "√од меньше или равен 0" };
+        throw std::invalid_argument{ "Год меньше или равен 0" };
     }
 }
 
 Date::Date(const int day, const int month, const int year)
 {
-    set_day(day);
     set_month(month);
+    set_day(day);
     set_year(year);
 }
 
